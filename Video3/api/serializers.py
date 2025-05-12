6eaa0dfd3a4b8eb8059510cacbd36f904b86923d
Ubuntu User <ubuntuuser@ubuntuserver.com>
2025-05-12 03:45:17 +0200

    Video 3 - Part 1

---
+++ Video3/api/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from .models import Product, Order, OrderItem
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'description',
+            'price',
+            'stock',
+        )
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                "Price must be greater than zero."
+            )
+        return value
+    
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = (
+            'product',
+            'quantity',
+        )
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)    
+    total_price = serializers.SerializerMethodField(method_name='total')
+
+    def total(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
+    class Meta:
+        model = Order
+        fields = (
+            'order_id',
+            'created_at',
+            'user',
+            'status',
+            'items',
+            'total_price',
+        )
+
+    
