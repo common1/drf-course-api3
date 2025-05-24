52b2b4c3f8cdf2b80249c0210f67d6b261e8cae5
Ubuntu User <ubuntuuser@ubuntuserver.com>
2025-05-24 21:52:42 +0200

    Video 19 - Part 1

---
+++ Video19/api/filters.py
@@ -0,0 +1,18 @@
+import django_filters # type: ignore
+
+from api.models import Product
+from rest_framework import filters;
+
+class InStockFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        return queryset.filter(stock__gt=0)
+        # return queryset.exclude(stock__gt=0)
+
+class ProductFilter(django_filters.FilterSet):
+    class Meta:
+        model = Product
+        fields = {
+            'name': ['iexact', 'icontains'],
+            'price': ['iexact', 'lt', 'gt', 'range'],
+        }
+
