6d7a0131d1380ec370dadedb93b13b5e94a70faa
Ubuntu User <ubuntuuser@ubuntuserver.com>
2025-05-24 13:46:02 +0200

    Video 17 - Part 1

---
+++ Video17/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('products/', views.ProductListCreateAPIView.as_view(), name='product_list'),
+    path('products/info/', views.ProductInfoAPIView.as_view(), name='product_info'),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view(), name='product_detail'),
+    path('orders/', views.OrderListAPIView.as_view(), name='order_list'),
+    path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders'),
+]
