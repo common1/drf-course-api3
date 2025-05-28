d93031e1b0b2b57bc32fbb65cc58ddc46ddbc562
ubuntuuser <ubuntuuser@ubuntuserver.com>
2025-05-28 14:18:20 +0200

    Video 24 - Part 1

---
+++ Video24/api/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from api.models import (
+    Order,
+    OrderItem,
+    Product,
+    User,
+)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemInline
+    ]
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
+admin.site.register(Product)
+admin.site.register(User)
